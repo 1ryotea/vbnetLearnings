fba9fa67be8ae427f9637c05abf3c7dbc8de73e4
RYOTEA <hibbanfadluu@gmail.com>
2025-03-06 05:55:56 +0700

    Add project files.

---
+++ Kalkulator/Form1.Designer.vb
@@ -0,0 +1,366 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnAC = New System.Windows.Forms.Button()
+        Me.btnKali = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnBagi = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnMinus = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btnJWB = New System.Windows.Forms.Button()
+        Me.btnDOTS = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnPercents = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAC
+        '
+        Me.btnAC.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnAC.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAC.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAC.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnAC.Location = New System.Drawing.Point(-6, 78)
+        Me.btnAC.Name = "btnAC"
+        Me.btnAC.Size = New System.Drawing.Size(87, 83)
+        Me.btnAC.TabIndex = 0
+        Me.btnAC.Text = "AC"
+        Me.btnAC.UseVisualStyleBackColor = False
+        '
+        'btnKali
+        '
+        Me.btnKali.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnKali.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnKali.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnKali.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnKali.Location = New System.Drawing.Point(236, 78)
+        Me.btnKali.Name = "btnKali"
+        Me.btnKali.Size = New System.Drawing.Size(81, 83)
+        Me.btnKali.TabIndex = 3
+        Me.btnKali.Text = "x"
+        Me.btnKali.UseVisualStyleBackColor = False
+        '
+        'btnHapus
+        '
+        Me.btnHapus.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnHapus.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnHapus.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHapus.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnHapus.Location = New System.Drawing.Point(78, 78)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(81, 83)
+        Me.btnHapus.TabIndex = 2
+        Me.btnHapus.Text = "BckSpc"
+        Me.btnHapus.UseVisualStyleBackColor = False
+        '
+        'btnBagi
+        '
+        Me.btnBagi.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnBagi.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnBagi.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBagi.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnBagi.Location = New System.Drawing.Point(236, 157)
+        Me.btnBagi.Name = "btnBagi"
+        Me.btnBagi.Size = New System.Drawing.Size(81, 83)
+        Me.btnBagi.TabIndex = 7
+        Me.btnBagi.Text = "/"
+        Me.btnBagi.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn3.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn3.Location = New System.Drawing.Point(158, 157)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(81, 83)
+        Me.btn3.TabIndex = 6
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn2.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn2.Location = New System.Drawing.Point(78, 157)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(81, 83)
+        Me.btn2.TabIndex = 5
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn1.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn1.Location = New System.Drawing.Point(0, 157)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(81, 83)
+        Me.btn1.TabIndex = 4
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btnMinus
+        '
+        Me.btnMinus.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnMinus.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnMinus.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMinus.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnMinus.Location = New System.Drawing.Point(236, 236)
+        Me.btnMinus.Name = "btnMinus"
+        Me.btnMinus.Size = New System.Drawing.Size(81, 83)
+        Me.btnMinus.TabIndex = 11
+        Me.btnMinus.Text = "-"
+        Me.btnMinus.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn6.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn6.Location = New System.Drawing.Point(158, 236)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(81, 83)
+        Me.btn6.TabIndex = 10
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn5.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn5.Location = New System.Drawing.Point(78, 236)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(81, 83)
+        Me.btn5.TabIndex = 9
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn4.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn4.Location = New System.Drawing.Point(0, 236)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(81, 83)
+        Me.btn4.TabIndex = 8
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btnPlus
+        '
+        Me.btnPlus.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnPlus.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnPlus.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlus.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnPlus.Location = New System.Drawing.Point(236, 317)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(81, 83)
+        Me.btnPlus.TabIndex = 15
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn9.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn9.Location = New System.Drawing.Point(158, 317)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(81, 83)
+        Me.btn9.TabIndex = 14
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn7.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn7.Location = New System.Drawing.Point(0, 317)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(81, 83)
+        Me.btn7.TabIndex = 12
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn8.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn8.Location = New System.Drawing.Point(78, 317)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(81, 83)
+        Me.btn8.TabIndex = 13
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btnJWB
+        '
+        Me.btnJWB.BackColor = System.Drawing.Color.Gold
+        Me.btnJWB.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnJWB.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnJWB.ForeColor = System.Drawing.Color.White
+        Me.btnJWB.Location = New System.Drawing.Point(236, 396)
+        Me.btnJWB.Name = "btnJWB"
+        Me.btnJWB.Size = New System.Drawing.Size(81, 83)
+        Me.btnJWB.TabIndex = 19
+        Me.btnJWB.Text = "="
+        Me.btnJWB.UseVisualStyleBackColor = False
+        '
+        'btnDOTS
+        '
+        Me.btnDOTS.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnDOTS.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnDOTS.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDOTS.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnDOTS.Location = New System.Drawing.Point(158, 396)
+        Me.btnDOTS.Name = "btnDOTS"
+        Me.btnDOTS.Size = New System.Drawing.Size(81, 83)
+        Me.btnDOTS.TabIndex = 18
+        Me.btnDOTS.Text = "."
+        Me.btnDOTS.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btn0.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btn0.Location = New System.Drawing.Point(0, 396)
+        Me.btn0.Name = "btn0"
+        Me.btn0.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btn0.Size = New System.Drawing.Size(159, 83)
+        Me.btn0.TabIndex = 16
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'txtOutput
+        '
+        Me.txtOutput.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtOutput.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtOutput.ForeColor = System.Drawing.Color.White
+        Me.txtOutput.Location = New System.Drawing.Point(0, 0)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(316, 82)
+        Me.txtOutput.TabIndex = 20
+        Me.txtOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnPercents
+        '
+        Me.btnPercents.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.btnPercents.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnPercents.Font = New System.Drawing.Font("Arial", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPercents.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnPercents.Location = New System.Drawing.Point(158, 80)
+        Me.btnPercents.Name = "btnPercents"
+        Me.btnPercents.Size = New System.Drawing.Size(81, 83)
+        Me.btnPercents.TabIndex = 21
+        Me.btnPercents.Text = "%"
+        Me.btnPercents.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer), CType(CType(43, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(317, 476)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnJWB)
+        Me.Controls.Add(Me.btnDOTS)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnMinus)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnBagi)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnKali)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnAC)
+        Me.Controls.Add(Me.btnPercents)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(333, 515)
+        Me.MinimumSize = New System.Drawing.Size(333, 515)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAC As Button
+    Friend WithEvents btnKali As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnBagi As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnMinus As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btnJWB As Button
+    Friend WithEvents btnDOTS As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents btnPercents As Button
+End Class
---
+++ Kalkulator/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnDOTS_Click(sender As Object, e As EventArgs) Handles btnDOTS.Click
+        txtOutput.Text &= btnDOTS.Text
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtOutput.TextChanged
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtOutput.Text &= btn1.Text
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtOutput.Text &= btn0.Text
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtOutput.Text &= btn2.Text
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtOutput.Text &= btn3.Text
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtOutput.Text &= btn7.Text
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txtOutput.Text &= btn4.Text
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtOutput.Text &= btn5.Text
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtOutput.Text &= btn6.Text
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtOutput.Text &= btn8.Text
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtOutput.Text &= btn9.Text
+    End Sub
+
+    Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+        txtOutput.Text &= btnPlus.Text
+
+    End Sub
+
+    Private Sub btnJWB_Click(sender As Object, e As EventArgs) Handles btnJWB.Click
+        Try
+            Dim table As New DataTable()
+            Dim result As Object = table.Compute(txtOutput.Text, String.Empty)
+            txtOutput.Text = result.ToString()
+        Catch ex As Exception
+            txtOutput.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnBagi_Click(sender As Object, e As EventArgs) Handles btnBagi.Click
+        txtOutput.Text &= btnBagi.Text
+    End Sub
+
+    Private Sub btnMinus_Click(sender As Object, e As EventArgs) Handles btnMinus.Click
+        txtOutput.Text &= btnMinus.Text
+    End Sub
+
+    Private Sub btnKali_Click(sender As Object, e As EventArgs) Handles btnKali.Click
+        txtOutput.Text &= "*"
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If txtOutput.Text.Length > 0 Then
+            txtOutput.Text = txtOutput.Text.Substring(0, txtOutput.Text.Length - 1)
+        End If
+    End Sub
+
+    Private Sub btnAC_Click(sender As Object, e As EventArgs) Handles btnAC.Click
+        If txtOutput.Text.Length > 0 Then
+            txtOutput.Text = txtOutput.Text.Substring(0, txtOutput.Text.Length = 0)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPercents.Click
+        txtOutput.Text &= btnPercents.Text
+    End Sub
+End Class
+'JUST NOTE
+'1. Handles adalah Perintah yang digunakan untuk mengaktifkan event.
+'Contohnya Handles Btn1.Click = event akan dijalankan ketika btn1 diklik(btn1.click)
+
+'2. Dim adalah Declare in memory (Deklarasi dalam memori), biasa digunakan di belakang variabel
+'Contohnya Dim Nomor as integer = 1, jadi... Deklarasi(Dim) NamaVariabel(Nomor) sebagai(as) TipeData(Integer) = isiVariable(1)
+
+'3. DataTable adalah sebuah kelas yang digunakan untuk menyimpan data dalam bentuk tabel.
+'Kelas ini menyediakan berbagai metode untuk mengelola data dalam tabel, seperti menghitung, mengurutkan, dan mencari data.
+'Untuk sekarang, Aku cuma pakai Compute() untuk menghitung hasil dari inputan user pada txtOutput.Text
